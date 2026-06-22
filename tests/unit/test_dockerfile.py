376224aa3530234524204d148b485bc019874c8e
Kyle Armstrong <24781678+robarmstrong96@users.noreply.github.com>
2026-06-22 10:39:33 -0400

    Simplify Dockerfile comments

--- tests/unit/test_dockerfile.py
+++ tests/unit/test_dockerfile.py
@@ -16,22 +16,6 @@ def test_dockerfile_uses_single_centos_stream_arg() -> None:
     assert dockerfile.count("FROM ${CENTOS_STREAM_IMAGE}") == 4
 
 
-def test_dockerfile_redeclares_only_used_final_stage_args() -> None:
-    dockerfile = dockerfile_text()
-    final_stage = dockerfile.split("FROM ${CENTOS_STREAM_IMAGE}")[-1]
-
-    assert "ARG AWX_REF\n" not in final_stage
-    assert "ARG RECEPTOR_IMAGE\n" in final_stage
-    assert 'dev.awx-wrapper.receptor.image="${RECEPTOR_IMAGE}"' in final_stage
-
-
-def test_dockerfile_groups_runtime_environment() -> None:
-    dockerfile = dockerfile_text()
-
-    assert "ENV LANG=en_US.UTF-8 \\\n    LANGUAGE=en_US:en" in dockerfile
-    assert dockerfile.count("ENV LANG=en_US.UTF-8") == 2
-
-
 def test_dockerfile_does_not_use_remote_add_for_repo_files() -> None:
     dockerfile = dockerfile_text()
 
